acd4ca2f31612d5ce6daf83bac696c986906c63e
Anderson de Oliveira <anderson.java.dev@gmail.com>
2024-02-29 21:03:32 -0300

    first commit

---
+++ src/test/kotlin/com/aodev/avengers/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aodev.avengers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
